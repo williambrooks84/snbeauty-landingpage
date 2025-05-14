82adb93c041e207b312d1bbf5aa9f5107fb080b9
William Brooks <william.brooks@etu.unilim.fr>
2025-05-14 11:05:01 +0200

    main: first init

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  css: [
+    '@/assets/scss/global.scss',
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/assets/scss/variables.scss" as *;` 
+        }
+      }
+    }
+  }
+})
